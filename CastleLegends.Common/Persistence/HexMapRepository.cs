d265f44fc7c53afd212070f9f6d906400980d40c
mizrael <davide.guida@razorfishhealthware.com>
2013-11-18 18:15:47 +0100

    added HexMapRepository, first draft

---
+++ CastleLegends.Common/Persistence/HexMapRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+
+namespace CastleLegends.Common.Persistence
+{
+    public class HexMapRepository
+    {
+        public void Save(HexMap map, string fullPath)
+        {
+            if (null == map) 
+                throw new ArgumentNullException("map");
+            if (string.IsNullOrWhiteSpace(fullPath))
+                throw new ArgumentNullException("fullPath");
+        }
+
+        public HexMap Load(string fullPath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
